706a6693f542388bf27ca50838379f8947bfed4d
learner-22 <nbhooma@gmail.com>
2022-06-01 18:55:00 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.get('/:name',(req,res)=>{
     res.send(`<h1> Wow! Hello there,   ${ req.params.name}  !!!</h1>`)
 })
 
+let magicEight = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:question', (req,res)=>{
+    res.send(`<h1> Question: ${req.params.question} </h1>
+    <h2> Answer : ${magicEight[Math.floor(Math.random()*magicEight.length)]}</h2>`)
+})
 
 app.listen(PORT , (req,res)=>{
     console.log('Server  is Running...')
