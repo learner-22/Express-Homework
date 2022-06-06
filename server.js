2f1c0535771be379d11610bdd6a7cb9b0622064c
learner-22 <nbhooma@gmail.com>
2022-06-06 13:09:19 -0400

    Added Comments

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+
+//Initializing Express and setting PORT
 const express = require('express')
 const app = express()
-
 PORT =3000
 
+// Root route
 app.get('/' ,(req,res)=>{
     res.send('<h1>Hello, Stranger</h1>')
 })
 
 
-
+//Route to calculate tip when the Total amount and Tip amount are passes as params
 app.get('/tip/:num1/:num2',(req,res)=>{
 console.log(req.params.num2 , req.params.num1)
 let tip = (Number(req.params.num2)*Number(req.params.num1))/100
@@ -16,11 +18,14 @@ let tip = (Number(req.params.num2)*Number(req.params.num1))/100
     res.send(`<h1>Tip Amount :  ${tip} </h1>`)
 })
 
+//Route to say Welcome to the name passed as param
 app.get('/:name',(req,res)=>{
     
     res.send(`<h1> Wow! Hello there,   ${ req.params.name}  !!!</h1>`)
 })
 
+
+//Route to evaluate if a number is in the Fibonacci series
 app.get('/fibonacci/:n' ,(req,res)=>{
     
     let n = req.params.n
@@ -33,6 +38,8 @@ app.get('/fibonacci/:n' ,(req,res)=>{
     }
     
 })
+
+// Route for the Magic 8 ball
 let magicEight = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 app.get('/magic/:question', (req,res)=>{
@@ -40,6 +47,7 @@ app.get('/magic/:question', (req,res)=>{
     <h2> Answer : ${magicEight[Math.floor(Math.random()*magicEight.length)]}</h2>`)
 })
 
+//Listening to the port
 app.listen(PORT , (req,res)=>{
     console.log('Server  is Running...')
 })
--- takeone.js
+++ takeone.js
@@ -1,14 +1,31 @@
+
+//Initializing Express and setting PORT
 const express = require('express')
 const app = express()
 
 PORT =4000
 
+//Setting up the view engine
 app.set('view engine', 'ejs')
 app.set('views', './Views')
 
 
 let count = 99
-let style =''
+
+
+// Root route to start the game
+app.get('/' ,(req,res)=>{
+   // res.send('<h1>99 Bottles of beer on the wall</h1>')
+  
+     res.render('takeOne',{
+        pageTitle: 'Take One Down', 
+        pageHeader: '99 Bottles of beer on the wall',
+        index : 99
+   })
+})
+ 
+
+//Logic for takeone down Game
 app.get('/:id',(req,res) =>{
    
   count = req.params.id
@@ -24,7 +41,7 @@ app.get('/:id',(req,res) =>{
   else{
     res.render('stepdown',{
         pageTitle: 'Take One Down', 
-        pageHeader: 'Bottles of beer on the wall -- Start the Game',
+        pageHeader: 'Bottles of beer on the wall',
         index : 99  ,
         text : 'Start Over'
     })
@@ -34,19 +51,6 @@ app.get('/:id',(req,res) =>{
  
 })
   
-
-
-
-app.get('/' ,(req,res)=>{
-   // res.send('<h1>99 Bottles of beer on the wall</h1>')
-  
-     res.render('takeOne',{
-        pageTitle: 'Take One Down', 
-        pageHeader: '99 Bottles of beer on the wall',
-        index : 99
-   })
-})
- 
 app.listen(PORT , (req,res)=>{
     console.log('Server  is Running...')
 })
